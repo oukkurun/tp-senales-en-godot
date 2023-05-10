9028a047dae9f6bd89f7ac2a28ebf5d5a653d951
oukkurun <oukkurun@gmail.com>
2023-05-10 20:27:45 -0300

    función Count_new_instance
    se creó la función para contar nuevas instancias de bloques.

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -9,3 +9,7 @@ func _ready():
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
+
+func count_new_instance():
+	instancesCount += 1
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
